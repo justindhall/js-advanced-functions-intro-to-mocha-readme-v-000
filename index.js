d2e4304337ebbc119f8b1330ed6f7a8d8804ac09
Justin Hall <hall.justin.d@gmail.com>
2019-03-09 21:52:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 function favoriteIceCream(flavor) {
-  return `I love ${flavor}`
+  return `I love ${flavor}`;
+}
+
+function theTruth(){
+  return true;
+}
+
+function sayHeyFriend(friend){
+  return `Hey ${friend}`;
+}
+
+function sayHey(){
+  return 'Hey!';
+}
+
+function shouting(string){
+  return string.toUpperCase();
 }
